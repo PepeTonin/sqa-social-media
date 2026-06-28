c3bfbbe2919be8f7e2aef872c246216d6624fee3
= <mppozzobon8@gmail.com>
2026-06-27 22:26:23 -0300

    preparing pipeline

--- playwright.config.ts
+++ playwright.config.ts
@@ -61,9 +61,20 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: [
+    {
+      command: 'java -jar build/libs/api.jar',
+      cwd: './api',
+      url: 'http://localhost:8080',
+      reuseExistingServer: !process.env.CI,
+      timeout: 180_000,
+    },
+    {
+      command: 'npm run start',
+      cwd: './client',
+      url: 'http://localhost:3000',
+      reuseExistingServer: !process.env.CI,
+      timeout: 120_000,
+    },
+  ],
 });
